6798060841f3a12a41eaeff75e1e05e396b0abf9
Sofia Ayala <sofiarayala1995@gmail.com>
2025-06-12 18:11:41 +0200

    implemented desktop layout trips cards

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -34,6 +34,44 @@ export class CardComponent {
       backgroundImageUrl:
         'https://images.pexels.com/photos/2166822/pexels-photo-2166822.jpeg',
     },
+
+    {
+      name: 'Germany',
+      date: '2025-09-10',
+      backgroundImageUrl:
+        'https://images.pexels.com/photos/28954966/pexels-photo-28954966.jpeg',
+    },
+    {
+      name: 'Ecuador',
+      date: '2025-08-15',
+      backgroundImageUrl:
+        'https://images.pexels.com/photos/30785381/pexels-photo-30785381.jpeg',
+    },
+
+    {
+      name: 'Pirineos Aragoneses',
+      date: '2025-07-01',
+      backgroundImageUrl:
+        'https://images.pexels.com/photos/2166822/pexels-photo-2166822.jpeg',
+    },
+    {
+      name: 'Ecuador',
+      date: '2025-08-15',
+      backgroundImageUrl:
+        'https://images.pexels.com/photos/30785381/pexels-photo-30785381.jpeg',
+    },
+    {
+      name: 'Chamonix',
+      date: '2025-07-01',
+      backgroundImageUrl:
+        'https://images.pexels.com/photos/158089/aiguille-du-midi-chamonix-mountain-station-mont-blanc-158089.jpeg',
+    },
+    {
+      name: 'Germany',
+      date: '2025-09-10',
+      backgroundImageUrl:
+        'https://images.pexels.com/photos/28954966/pexels-photo-28954966.jpeg',
+    },
   ];
 
   getBackgroungImageStyle(url: string): string {
