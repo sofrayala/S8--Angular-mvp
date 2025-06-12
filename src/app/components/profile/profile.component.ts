e0e983ddfdfd215dacfff0b0e33fd426dd912833
Sofia Ayala <sofiarayala1995@gmail.com>
2025-06-12 11:49:48 +0200

    added home and profile components with routing

---
+++ src/app/components/profile/profile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+
+@Component({
+  selector: 'app-profile',
+  imports: [NavbarComponent],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.scss',
+})
+export class ProfileComponent {}
